9cc52c679026852e1cc82686ae664c20acfbd7dd
kylych-dev <mirbekov1kylych@gmail.com>
2024-05-22 16:49:04 +0600

    click

---
+++ README.md
@@ -0,0 +1,7 @@
+```bash
+poetry env use python3.12
+poetry install
+```
+```bash
+python main.py input_files/sample.html --max-length 500
+```
--- main.py
+++ main.py
@@ -3,15 +3,25 @@ from bs4 import BeautifulSoup as bs
 import re, os, click
 
 
-
-MAX_LENGTH = 300
-# MAX_LENGTH = 5
+MAX_LENGTH = 5000
 
 
+@click.command()
+@click.option('--max-length', type=int, default=MAX_LENGTH, help='The maximum length of the message fragment.')
+@click.argument('source', type=click.Path(exists=True))
+def main_func(source, max_length):
+    output_directory = 'output_files'
+    base_name = 'sample'
+    extension = '.html'
 
-# @click.command()
-# @click.option('--source', '-s', help='Path to the source file')
-# @click.argument('source', type=click.Path(exists=True))
+    next_filename = get_next_filename(output_directory, base_name, extension)
+
+    res2 = split_message('input_files/sample.html')
+    with open(os.path.join(output_directory, next_filename), 'w', encoding='utf-8') as file:
+        for part in res2:
+            file.write(part + '\n')
+
+
 def split_message(source: str, max_length=MAX_LENGTH) -> Generator[str, None, None]:
     """Splits the original message (`source`) into fragments of the specified length
         (`max_len`)."""
@@ -66,13 +76,6 @@ def get_next_filename(directory: str, base_name: str, extension: str) -> str:
 
 
 if __name__ == '__main__':
-    output_directory = 'output_files'
-    base_name = 'sample'
-    extension = '.html'
+    main_func()
 
-    next_filename = get_next_filename(output_directory, base_name, extension)
 
-    res2 = split_message('input_files/sample.html')
-    with open(os.path.join(output_directory, next_filename), 'w', encoding='utf-8') as file:
-        for part in res2:
-            file.write(part + '\n')
